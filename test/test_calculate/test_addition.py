77c492d030e8c56d036e305bf295bd91842b8461
MH-Tobi <pflug_tobias@live.de>
2026-06-16 09:42:07 +0200

    simplify test-names

--- test/test_calculate/test_addition.py
+++ test/test_calculate/test_addition.py
@@ -3,42 +3,42 @@ from test.helper_functions import test_helper_functions as helper_functions
 
 
 # Test Addition
-def test_addition_two_positive_integers():
+def test_two_positive_integers():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "1+3", '4\n')
 
-def test_addition_three_positive_integers():
+def test_three_positive_integers():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "1+3+5", '9\n')
 
-def test_addition_two_negative_integers():
+def test_two_negative_integers():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-1+-2", '-3\n')
 
-def test_addition_three_negative_integers():
+def test_three_negative_integers():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-2+-3+-6", '-11\n')
 
-def test_addition_positive_and_negative_integers():
+def test_positive_and_negative_integers():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-2+3+6+-15+-1", '-9\n')
 
-def test_addition_two_positive_floats():
+def test_two_positive_floats():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "0.1+3.7", '3.8\n')
 
-def test_addition_three_positive_floats():
+def test_three_positive_floats():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "0.23+3.45+5.01", '8.69\n')
 
-def test_addition_two_negative_floats():
+def test_two_negative_floats():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-1.2+-2.03", '-3.23\n')
 
-def test_addition_three_negative_floats():
+def test_three_negative_floats():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-2.7+-3.73+-6.15", '-12.58\n')
 
-def test_addition_positive_and_negative_floats():
+def test_positive_and_negative_floats():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-2.01+3.645+6.78+-1.545+-0.01", '6.86\n')
--- test/test_calculate/test_substraction.py
+++ test/test_calculate/test_substraction.py
@@ -3,42 +3,42 @@ from test.helper_functions import test_helper_functions as helper_functions
 
 
 # Test Substraction
-def test_substraction_two_positive_integers():
+def test_two_positive_integers():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "10-3", '7\n')
 
-def test_substraction_three_positive_integers():
+def test_three_positive_integers():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "10-2-5", '3\n')
 
-def test_substraction_two_negative_integers():
+def test_two_negative_integers():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-12--1", '-11\n')
 
-def test_substraction_three_negative_integers():
+def test_three_negative_integers():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-1--12--3", '14\n')
 
-def test_substraction_positive_and_negative_integers():
+def test_positive_and_negative_integers():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "2-3-6--15--1", '9\n')
 
-def test_substraction_two_positive_floats():
+def test_two_positive_floats():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "0.15-4.35", '-4.2\n')
 
-def test_substraction_three_positive_floats():
+def test_three_positive_floats():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "0.23-3.45-5.01", '-8.23\n')
 
-def test_substraction_two_negative_floats():
+def test_two_negative_floats():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-1.2--2.03", '0.83\n')
 
-def test_substraction_three_negative_floats():
+def test_three_negative_floats():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-2.7--3.73--6.15", '7.18\n')
 
-def test_substraction_positive_and_negative_floats():
+def test_positive_and_negative_floats():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-2.01-3.645-6.78--1.545--0.01", '-10.88\n')
