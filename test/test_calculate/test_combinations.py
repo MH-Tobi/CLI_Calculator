0a7cfafae77ceaf671d73e9a13e9d3fb842c974b
MH-Tobi <pflug_tobias@live.de>
2026-06-16 09:44:18 +0200

    - simplify test-names - added new tests for combination of integers and floats

--- test/test_calculate/test_combinations.py
+++ test/test_calculate/test_combinations.py
@@ -3,18 +3,26 @@ from test.helper_functions import test_helper_functions as helper_functions
 
 
 # Test combinations of multiple types
-def test_combination_integer_1():
+def test_integer_1():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-2+10*2/5-15+-8--9", '-12.0\n')
 
-def test_combination_integer_2():
+def test_integer_2():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "7*-4--15*2+3/-5/6", '1.9\n')
 
-def test_combination_floats_1():
+def test_floats_1():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "-0.2+-10.236*2.645/-0.565-1.655", '46.0639734513\n')
 
-def test_combination_floats_2():
+def test_floats_2():
     runner = CliRunner()
     helper_functions._calculate_test_helper(runner, "1.54*-6.68/-46.65+45.977*1235.201354", '56791.0731716147\n')
+
+def test_integers_and_floats_1():
+    runner = CliRunner()
+    helper_functions._calculate_test_helper(runner, "22*15.875-54.87/-65-66+-88.565", '195.5291538462\n')
+
+def test_integers_and_floats_2():
+    runner = CliRunner()
+    helper_functions._calculate_test_helper(runner, "-689.0021*-0.003/2/2", '0.516751575\n')
